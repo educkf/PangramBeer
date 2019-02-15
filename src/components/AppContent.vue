d6f016a2b8ded78c1feb559fdeea36f87d9e91bd
Eduardo Ferreira <eduardo.ckf@gmail.com>
2019-02-14 23:47:12 -0200

    Hints

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -11,13 +11,18 @@
             
             <AlphabetChecker :pangram="pangram" @typed="typed" @backspace="backspace" @highlight="highlighted = $event" :highlight="highlighted" />
 
-            <div class="suggestions">
-                <span v-for="suggestion in suggestions" :key="suggestion" @click="addWord(suggestion)" class="suggestion">{{ suggestion }}</span>
+            <div class="suggestions" v-if="suggestions.length > 0">
+                <span v-for="suggestion in suggestions" :key="suggestion" @click="addWord(suggestion, true)" class="suggestion">{{ suggestion }}</span>
+            </div>
+
+            <div class="suggestions" v-if="hints.length > 0">
+                <span v-for="hint in hints" :key="hint" @click="addWord(hint, false)" class="suggestion">{{ hint }}</span>
             </div>
         </div>
 
         <div class="flex buttons-area">
             <div class="btn clear" @click="clear()">Clear</div>
+            <div class="btn suggestion" @click="hint()">Suggestion</div>
             <div class="btn" @click="save()">Save</div>
         </div>
         
@@ -53,7 +58,8 @@ export default {
             pangram: '',
             pangrams: [],
             highlighted: '',
-            dictionary: ['Acetaldehyde', 'Acid Rest', 'Acrospire', 'Adjunct', 'Aeration', 'Alcohol', 'ABV', 'Alcoholic', 'Ale', 'Airlock', 'Saccharomyces cerevisiae', 'Extract', 'Alpha Acid', 'Alpha Amylase', 'Beta Amylase', 'Aroma', 'Astringency', 'Attenuation', 'Barley', 'Barrel', 'Beta Acids', 'Bitterness', 'IBU', 'BJCP', 'Body', 'Boiling', 'Bottle', 'Bottom Fermentation', 'Brettanomyces', 'Brewpub', 'Brew Kettle', 'Sulphur', 'Carbon Dioxide', 'Carbonation', 'Cellaring', 'Chill', 'Haze', 'Hazy', 'Cold Break', 'Color', 'Craft', 'Decoction Mash', 'Plato', 'Dextrin', 'Diacetyl', 'Diastatic', 'Dry Hopping', 'Essential Hop Oils', 'Esters', 'Ethanol', 'Fermentable Sugars', 'Fermentation', 'Fermentation Lock', 'Filtration', 'Filter', 'Final gravity', 'Flocculation', 'Forced Carbonation', 'Fresh Hopping', 'Germination', 'Grainy', 'Grist', 'Growler', 'Gruit', 'Head Retention', 'Homebrewing', 'Hops', 'Hopping', 'Humulene', 'Hydrometer', 'Immersion Chiller', 'Infusion', 'Inoculate', 'Irish Moss', 'Gelatine', 'Keg', 'Kraeusen', 'Lactobacillus', 'Lager', 'Lagering', 'Lightstruck', 'Liquorous', 'Lovibond', 'Malt', 'Malt Extract', 'Maltose', 'Mash', 'Mashing', 'Mash Out', 'Microbrewery', 'Mouthfeel', 'Musty', 'Myrcene', 'Natural Carbonation', 'Ninkasi', 'Nitrogen', 'Noble Hops', 'Oasthouse', 'Original Gravity', 'OG', 'Oxidation', 'pH', 'Phenols', 'Pitching', 'Primary Fermentation', 'Priming', 'Prohibition', 'Racking', 'Real Ale', 'Residual', 'Alkalinity', 'Residual Sugar', 'Resin', 'Saccharification', 'Saccharomyces', 'Secondary Fermentation', 'Session Beer', 'Solvent', 'Sorghum', 'Sour', 'Sparging', 'Specific Gravity', 'Sulfur Aroma', 'Tannins', 'Temperature Rests', 'Top Fermentation', 'Trigeminal Nerves', 'Trub', 'Turbidity', 'Volatile', 'Water', 'Wet Hopping', 'Whirlpool', 'Wort', 'Yeast', 'Zymurgy', 'Admiral', 'Agnus', 'Ahtanum', 'AlphAroma', 'Amarillo', 'Amethyst', 'Apollo', 'Aramis', 'Atlas', 'Aurora', 'Beata', 'Belma', 'Bitter Gold', 'Boadicea', 'Bobek', 'Bouclier', 'Bramling Cross', 'Bravo', 'Brewers Gold', 'British Kent Goldings', 'Bullion', 'Calicross', 'California Cluster', 'Calypso', 'Cascade', 'Cashmere', 'Cekin', 'Celeia', 'Centennial', 'Challenger', 'Chelan', 'Chinook', 'Cicero', 'Citra', 'Cluster', 'Cobb’s Golding', 'Columbia', 'Columbus', 'Comet', 'Dana', 'Delta', 'Dr. Rudi', 'Early Green', 'El Dorado', 'Ella', 'Endeavour', 'Equinox', 'Eroica', 'Falconers Flight', 'First Gold', 'Flyer', 'Fuggle', 'Galaxy', 'Galena', 'Glacier', 'Golding', 'Green Bullet', 'Hallertau', 'HBC 342 Experimental', 'Helga', 'Herald', 'Herkules', 'Hersbrucker', 'Horizon', 'Huell Melon', 'Ivanhoe', 'Jester', 'Junga', 'Kazbek', 'Kohatu', 'Liberty', 'Lubelski', 'Magnum', 'Mandarina Bavaria', 'Mathon', 'Marynka', 'Meridian', 'Merkur', 'Millennium', 'Mittelfruh', 'Mosaic', 'Motueka', 'Mt. Hood', 'Mt. Rainier', 'Nelson Sauvin', 'Newport', 'Northdown', 'Northern Brewer', 'Nugget', 'Opal', 'Orbit', 'Orion', 'Outeniqua', 'Pacific Gem', 'Pacific Jade', 'Pacific Sunrise', 'Pacifica', 'Palisade', 'Perle', 'Phoenix', 'Pilgrim', 'Pilot', 'Pioneer', 'Polaris', 'Premiant', 'Pride of Ringwood', 'Progress', 'Rakau', 'Riwaka', 'Saaz', 'Santiam', 'Saphir', 'Satus', 'Select', 'Serebrianka', 'Simcoe', 'Sladek', 'Smaragd', 'Sonnet', 'Sorachi Ace', 'Southern Brewer', 'Southern Cross', 'Southern Promise', 'Southern Star', 'Sovereign', 'Spault', 'Spaulter Select', 'Sterling', 'Strickelbract', 'Strisselspault', 'Styrian Gold', 'Styrian Golding', 'Summer', 'Summit', 'Super Galena', 'Super Pride', 'Sussex', 'Sybilla', 'Sylva', 'Tahoma', 'Tardif de Burgogne', 'Target', 'Taurus', 'Teamaker', 'Tettnanger', 'Tillicum', 'Topaz', 'Tradition', 'Triple Pearl', 'Triskel', 'Ultra', 'Universal', 'Vanguard', 'Victoria', 'Viking', 'Vital', 'Vojvodina', 'Wai-iti', 'Waimea', 'Wakatu', 'Warrior', 'Whitbread Goldings', 'Willamette', 'Yakima Cluster', 'Yakima Gold', 'Zenith', 'Zythos', 'Pale Malt', 'Wheat Malt', 'Rye Malt', 'Vienna Malt', 'Munich Malt', 'Carapils', 'Caramel', 'Crystal', 'Victory Malt', 'Special Roast', 'Chocolate Malt', 'Roasted Barley', 'Black Barley', 'Weizen', 'Melanoidin', 'Amber Malt', 'Aromatic', 'Belgian Special', 'Biscuit Malt', 'CaraVienne', 'CaraMunich', 'Rauchmalt']
+            dictionary: ['Acetaldehyde', 'Acid Rest', 'Acrospire', 'Adjunct', 'Aeration', 'Alcohol', 'ABV', 'Alcoholic', 'Ale', 'Airlock', 'Saccharomyces cerevisiae', 'Extract', 'Alpha Acid', 'Alpha Amylase', 'Beta Amylase', 'Aroma', 'Astringency', 'Attenuation', 'Barley', 'Barrel', 'Beta Acids', 'Bitterness', 'IBU', 'BJCP', 'Body', 'Boiling', 'Bottle', 'Bottom Fermentation', 'Brettanomyces', 'Brewpub', 'Brew Kettle', 'Sulphur', 'Carbon Dioxide', 'Carbonation', 'Cellaring', 'Chill', 'Haze', 'Hazy', 'Cold Break', 'Color', 'Craft', 'Decoction Mash', 'Plato', 'Dextrin', 'Diacetyl', 'Diastatic', 'Dry Hopping', 'Essential Hop Oils', 'Esters', 'Ethanol', 'Fermentable Sugars', 'Fermentation', 'Fermentation Lock', 'Filtration', 'Filter', 'Final gravity', 'Flocculation', 'Forced Carbonation', 'Fresh Hopping', 'Germination', 'Grainy', 'Grist', 'Growler', 'Gruit', 'Head Retention', 'Homebrewing', 'Hops', 'Hopping', 'Humulene', 'Hydrometer', 'Immersion Chiller', 'Infusion', 'Inoculate', 'Irish Moss', 'Gelatine', 'Keg', 'Kraeusen', 'Lactobacillus', 'Lager', 'Lagering', 'Lightstruck', 'Liquorous', 'Lovibond', 'Malt', 'Malt Extract', 'Maltose', 'Mash', 'Mashing', 'Mash Out', 'Microbrewery', 'Mouthfeel', 'Musty', 'Myrcene', 'Natural Carbonation', 'Ninkasi', 'Nitrogen', 'Noble Hops', 'Oasthouse', 'Original Gravity', 'OG', 'Oxidation', 'pH', 'Phenols', 'Pitching', 'Primary Fermentation', 'Priming', 'Prohibition', 'Racking', 'Real Ale', 'Residual', 'Alkalinity', 'Residual Sugar', 'Resin', 'Saccharification', 'Saccharomyces', 'Secondary Fermentation', 'Session Beer', 'Solvent', 'Sorghum', 'Sour', 'Sparging', 'Specific Gravity', 'Sulfur Aroma', 'Tannins', 'Temperature Rests', 'Top Fermentation', 'Trigeminal Nerves', 'Trub', 'Turbidity', 'Volatile', 'Water', 'Wet Hopping', 'Whirlpool', 'Wort', 'Yeast', 'Zymurgy', 'Admiral', 'Agnus', 'Ahtanum', 'AlphAroma', 'Amarillo', 'Amethyst', 'Apollo', 'Aramis', 'Atlas', 'Aurora', 'Beata', 'Belma', 'Bitter Gold', 'Boadicea', 'Bobek', 'Bouclier', 'Bramling Cross', 'Bravo', 'Brewers Gold', 'British Kent Goldings', 'Bullion', 'Calicross', 'California Cluster', 'Calypso', 'Cascade', 'Cashmere', 'Cekin', 'Celeia', 'Centennial', 'Challenger', 'Chelan', 'Chinook', 'Cicero', 'Citra', 'Cluster', 'Cobb’s Golding', 'Columbia', 'Columbus', 'Comet', 'Dana', 'Delta', 'Dr. Rudi', 'Early Green', 'El Dorado', 'Ella', 'Endeavour', 'Equinox', 'Eroica', 'Falconers Flight', 'First Gold', 'Flyer', 'Fuggle', 'Galaxy', 'Galena', 'Glacier', 'Golding', 'Green Bullet', 'Hallertau', 'HBC 342 Experimental', 'Helga', 'Herald', 'Herkules', 'Hersbrucker', 'Horizon', 'Huell Melon', 'Ivanhoe', 'Jester', 'Junga', 'Kazbek', 'Kohatu', 'Liberty', 'Lubelski', 'Magnum', 'Mandarina Bavaria', 'Mathon', 'Marynka', 'Meridian', 'Merkur', 'Millennium', 'Mittelfruh', 'Mosaic', 'Motueka', 'Mt. Hood', 'Mt. Rainier', 'Nelson Sauvin', 'Newport', 'Northdown', 'Northern Brewer', 'Nugget', 'Opal', 'Orbit', 'Orion', 'Outeniqua', 'Pacific Gem', 'Pacific Jade', 'Pacific Sunrise', 'Pacifica', 'Palisade', 'Perle', 'Phoenix', 'Pilgrim', 'Pilot', 'Pioneer', 'Polaris', 'Premiant', 'Pride of Ringwood', 'Progress', 'Rakau', 'Riwaka', 'Saaz', 'Santiam', 'Saphir', 'Satus', 'Select', 'Serebrianka', 'Simcoe', 'Sladek', 'Smaragd', 'Sonnet', 'Sorachi Ace', 'Southern Brewer', 'Southern Cross', 'Southern Promise', 'Southern Star', 'Sovereign', 'Spault', 'Spaulter Select', 'Sterling', 'Strickelbract', 'Strisselspault', 'Styrian Gold', 'Styrian Golding', 'Summer', 'Summit', 'Super Galena', 'Super Pride', 'Sussex', 'Sybilla', 'Sylva', 'Tahoma', 'Tardif de Burgogne', 'Target', 'Taurus', 'Teamaker', 'Tettnanger', 'Tillicum', 'Topaz', 'Tradition', 'Triple Pearl', 'Triskel', 'Ultra', 'Universal', 'Vanguard', 'Victoria', 'Viking', 'Vital', 'Vojvodina', 'Wai-iti', 'Waimea', 'Wakatu', 'Warrior', 'Whitbread Goldings', 'Willamette', 'Yakima Cluster', 'Yakima Gold', 'Zenith', 'Zythos', 'Pale Malt', 'Wheat Malt', 'Rye Malt', 'Vienna Malt', 'Munich Malt', 'Carapils', 'Caramel', 'Crystal', 'Victory Malt', 'Special Roast', 'Chocolate Malt', 'Roasted Barley', 'Black Barley', 'Weizen', 'Melanoidin', 'Amber Malt', 'Aromatic', 'Belgian Special', 'Biscuit Malt', 'CaraVienne', 'CaraMunich', 'Rauchmalt'],
+            hints: []
         }
     },
     computed: {
@@ -102,10 +108,31 @@ export default {
                 this.pangram = this.pangram + ' '
             }
         },
-        addWord: function(word) {
+        addWord: function(word, replace = false) {
             const toArray = this.pangram.split(' ');
-            toArray[toArray.length - 1] = word + ' ';
+            if (replace) {
+                toArray[toArray.length - 1] = word + ' ';
+            } else {
+                toArray.push(word)
+            }
             this.pangram = toArray.join(' ')
+        },
+        hint() {
+            const alphabet = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+            const toArray = this.pangram.split('').map(item => item.toLowerCase());
+            const missingLetters = alphabet.filter(item => !toArray.includes(item.toLowerCase()))
+            if (missingLetters.length > 0) {
+                const allLetters = this.dictionary.filter(item => {
+                    const hints = missingLetters.filter(missing => {
+                        return item.toLowerCase().search(missing.toLowerCase()) > -1
+                    })
+                    return missingLetters.length <= 4 ? hints.length == missingLetters.length : hints.length > missingLetters.length - 3
+                })
+                this.hints = allLetters
+                setTimeout(() => {
+                    this.hints = []
+                }, 3000)
+            }
         }
     },
     created() {
@@ -192,6 +219,12 @@ export default {
     &.clear {
         border-color: $gray;
         color: $gray;
+    }
+
+    &.suggestion {
+        width: 180px;
+        background-color: $color;
+        color: #fff;
     }
 }
 
